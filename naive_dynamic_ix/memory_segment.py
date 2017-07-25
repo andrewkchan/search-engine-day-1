f4d2223bbca051b1707c25c80d580f36df789cae
Andrew Chan <andrewkchan@berkeley.edu>
2017-07-25 00:04:28 -0700

    phrase query prelim code

--- naive_dynamic_ix/memory_segment.py
+++ naive_dynamic_ix/memory_segment.py
@@ -7,6 +7,7 @@ License: MIT License
 from collections import defaultdict
 from bisect import bisect_left
 
+
 class Posting:
     def __init__(self, doc_id: int, positions: list):
         '''
@@ -46,12 +47,13 @@ class Posting:
                 j += 1
         self.positions = merged
 
+
 class PostingList:
-    def __init__(self, postings = []):
+    def __init__(self, postings = None):
         '''
         A PostingList maintains a list of Posting objects sorted by doc_id.
         '''
-        self.postings = postings
+        self.postings = postings if postings else []
 
         # keep a separate list of doc ids mostly for convenience to use with bisect_left.
         self._doc_ids = [posting.doc_id for posting in postings]
@@ -72,26 +74,46 @@ class PostingList:
             self.postings[posting_i].merge_posting(posting)
 
     @staticmethod
-    def find_positional_runs(posting_lists: list):
+    def find_phrases(posting_lists):
         '''
-        Finds postings that recur throughout the posting lists to form positional runs.
-        If all posting lists feature a doc id, and sequential posting lists
-        feature that doc id in sequential positions, that forms a positional run. Example:
+        Finds postings that recur throughout all the posting lists to form positional runs (phrases).
+        For any given document id, a positional run is formed when a number of posting lists each contain
+        that document id, and in sequential positions. Example:
         >>> x = PostingList([Posting(1, [2, 5]), Posting(2, [2])])
         >>> y = PostingList([Posting(1, [6])])
         >>> z = PostingList([Posting(1, [7]), Posting(2, [3])])
-        >>> find_positional_runs([x, y, z])
+        >>> PostingList.find_phrases(iter([x, y, z]))
         >>>   PostingList([Posting(1, [5])])
-        This is the basis for an "exact phrase" query.
-        :param posting_lists:
-        :return: PostingList.
+        This is the basis for an "exact phrase" query. We can find the occurrence of the exact phrase "Peter Piper Pied"
+        by finding positional runs for [PostingList("Peter"), PostingList("Piper"), PostingList("Pied")].
+        :param posting_lists: Iterator where each element is a PostingList.
+        :return: PostingList. The Postings contain start positions of found phrases.
         '''
-        if len(posting_lists) == 0:
-            return PostingList()
-        elif len(posting_lists) == 1:
-            return posting_lists[0]
+        first_posting_list = posting_lists.__next__()
+        # create forward index with doc_id as key and possible start positions of the phrase as values.
+        fw_index = {posting.doc_id: set(posting.positions) for posting in first_posting_list}
+        doc_ids = set(fw_index.keys())
 
+        for i, posting_list in enumerate(posting_lists):
+            ith_doc_ids = set()
+            for posting in posting_list:
+                doc_id = posting.doc_id
+                ith_doc_ids.add(doc_id)
+                if doc_id in fw_index:
+                    # in a phrase, the ith term must occur i-1 spots after the 1st term.
+                    offset_poses = [pos-(i-1) for pos in posting.positions]
+                    # discard possible phrase starts that don't coincide with any postings of the ith term.
+                    fw_index[doc_id] &= set(offset_poses)
+                    if len(fw_index[doc_id]) == 0:
+                        del fw_index[doc_id]
+            # discard doc ids where the ith phrase term does not occur
+            bad_doc_ids = doc_ids - ith_doc_ids
+            for doc_id in bad_doc_ids:
+                del fw_index[doc_id]
 
+        # any non-empty entries in the dictionary indicate documents where the phrase occurs.
+        result_postings = [Posting(item[0], sorted(item[1])) for item in fw_index.items()]
+        return PostingList(result_postings)
 
 from naive_dynamic_ix.results import Results
 class MemorySegment:
@@ -115,8 +137,10 @@ class MemorySegment:
         :param terms: List of strings representing the exact phrase in order.
         :return: Results object.
         '''
-        posting_list = self.index[terms[0]]
-        # TODO
+        term_postlists = [self.index[term] for term in terms]
+        phrase_postings = PostingList.find_phrases(iter(term_postlists)).postings
+        doc_ids = [posting.doc_id for posting in phrase_postings]
+        return Results(doc_ids)
 
     def add_token(self, term: str, doc_id: int, position: int):
         '''
